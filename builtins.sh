7d7e45dd966d5fd4d48c8ba838ad9bb427726008
vinesh-paul <65432432+vinesh-paul@users.noreply.github.com>
2021-08-29 20:08:48 +0530

    Create builtins.sh

---
+++ builtins.sh
@@ -0,0 +1,8 @@
+
+# ':' ( colon ) is a builtin that relates to truth value and is faster
+# This can be used in situations where we only need to set defaults or
+# parameter substitution and are not interested in the output
+# parameters/defaults are passed as argument to ':'
+# exit status is set to 0
+
+: {PLATFORM:=REDHAT7}
